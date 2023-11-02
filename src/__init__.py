60573aae21daebf8c12107f7cd2d2ef31edaa554
thotenn <joseenriquegaray@hotmail.com>
2023-11-02 13:42:33 -0300

    reestructuring

--- src/__init__.py
+++ src/__init__.py
@@ -8,13 +8,14 @@ from .coretool import (GenView, api_generate_serializer_class, generate_gen_view
                        dt_get_dayname, dt_are_these_dates_intersected, dt_difftimedelta_str, get_data_model_bd_set,
                        set_attribute, admin_descripcion, admin_ordenarpor, CsvImportForm, CsvExportMixin, CsvImport,
                        empaquetar_zip, get_size_mb, image_compress, image_rename_for_upload, image_name_for_save,
-                       images_compress_save,
-                       img_app_resize_save, image_bytearray_to_img, b64_to_img, image_get_content_file_from_bytearray,
-                       tools_json_dict_merge, tools_json_dict_all_to_str, net_get_abs_url,
+                       images_compress_save, img_app_resize_save, image_bytearray_to_img, b64_to_img,
+                       image_get_content_file_from_bytearray, tools_json_dict_merge, tools_json_dict_all_to_str,
+                       net_get_abs_url,
                        tools_objects_getattributes_values, tools_objects_getmembers, ZipResponse, strings_just_numbers,
                        strings_just_letters, strings_currency_format, strings_currency_gs,
                        strings_get_fcurrency_value, strings_get_s_numer, strings_array_values_to_str,
                        strings_generate_code, strings_generate_token_urlsafe, strings_generate_random_password,
                        strings_normalize_str, strings_normalize_slower_str, strings_delete_duplicates,
                        strings_seeker_iregex_and, strings_seeker_iregex_or, validator_no_espacios,
-                       validators_throw_error_basic)
+                       validators_throw_error_basic, ApiBasicController, ApiRestBaseController,
+                       ApiRestAbstractController, ApiRestStandard, PROCESS_TYPES, CrudBasic)
---
+++ src/coretool/apitools/__init__.py
@@ -0,0 +1,5 @@
+from .apibasiccontroller import ApiBasicController
+from .ApiRestAbstractController import ApiRestAbstractController
+from .apirestbase import ApiRestBaseController
+from .ApiRestStandard import ApiRestStandard
+from .crudbasic import PROCESS_TYPES, CrudBasic
